fe8e0766c6fbdd9fdc7d327d6c50865650d84d85
Chahed10 <chahedchaabene10@gmail.com>
2025-04-28 12:48:57 +0100

    les tableaux

---
+++ les tableaux/src/application 3.java
@@ -0,0 +1,23 @@
+/**
+ * @(#)application 3.java
+ *
+ *
+ * @author 
+ * @version 1.00 2025/4/24
+ */
+
+public class application 3 {
+        
+    /**
+     * Creates a new instance of <code>application 3</code>.
+     */
+    public application 3() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ les tableaux/src/les_tableaux.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)les_tableaux.java
+ *
+ * les_tableaux application
+ *
+ * @author 
+ * @version 1.00 2025/4/24
+ */
+ 
+public class les_tableaux {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
